d321f872d76a58cb17541a3718b68ee9a3764e9d
OmarAkhamlich <oakhamlichco24wg@ikzubirimanteo.com>
2025-12-01 13:45:26 +0100

    Footer + navbar hobetuta

--- car_crashers_ui/src/components/layout/layout.js
+++ car_crashers_ui/src/components/layout/layout.js
@@ -1,18 +1,31 @@
 //import React from "react";
-import Logo from '../../assets/images/logo.jpg';
+import Logo from "../../assets/images/logo.jpg";
+import NavBar from "../navBar.js";
+import React from "react";
+import "./layout.css";
 
 function Layout({ children }) {
   return (
     <div class="row">
-      <header class="bg-dark text-white p-3 mb-4">
+      <header class="text-white p-3 mb-4 header-orange d-flex flex-row justify-content-between">
         <div>
-          <img src={Logo} alt="Logo"></img>
-          <h1>CarCrashers</h1>
-          <p class="fst-italic fw-bolder">Drive more, crash less</p>
+          <img class="h-50" src={Logo} alt="Logo"></img>
+          {/* ¿Aqui el nombre? O solo en el navBar*/}
         </div>
+        <React.StrictMode>
+          <NavBar />
+        </React.StrictMode>
       </header>
 
       <main style={{ padding: "1rem" }}>{children}</main>
+      <footer class="bg-dark text-white">
+        <h1>Footer</h1>
+        <p>A</p>
+        <p>A</p>
+        <p>A</p>
+        <p>A</p>
+
+      </footer> 
     </div>
   );
 }
---
+++ car_crashers_ui/src/components/navBar.js
@@ -0,0 +1,41 @@
+import './layout/layout.css';
+
+
+
+function navBar() {
+  return (
+    <nav class="navbar navbar-expand-lg bg-body-tertiary rounded-5 h-25 w-50">
+        <div class="container d-flex flex-row justify-content-between align-items-center w-100 h-100">
+            <a class="navbar-brand" href="#">CarCrashers</a>
+            <p className="text-black fst-italic">Drive more, crash less</p>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div class="navbar-nav">
+                <a class="nav-link" href="#">
+                    <i class="bi bi-house-door-fill text-black">Home</i>      
+                </a>
+                 <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                         <i class="bi bi-currency-euro text-black"></i>
+                        Salerosketa
+                    </a>
+                    <ul class="dropdown-menu">
+                        <li><a class="dropdown-item" href="#">Erosi</a></li>
+                        <li><a class="dropdown-item" href="#">Saldu</a></li>
+                    </ul>
+                </li>
+
+                <a class="nav-link" href="#">
+                <i class="bi bi-buildings-fill">Nor gara?</i>
+                </a>
+                <button class="btn btn-outline-dark bg-orange" type="submit">Hasi Saioa </button>
+            </div>
+            </div>
+        </div>
+    </nav>
+  );
+}
+
+export default navBar;
