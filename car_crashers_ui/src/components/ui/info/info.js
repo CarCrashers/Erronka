acc4622b4998466ae66b9049eaaf80055252fe93
aezkerrabe25 <aezkerrabe25@ikzubirimanteo.com>
2025-12-12 13:53:04 +0100

    heroe-ko botoiak aldatu, bat gehitu, hover jarri + info konponentea sortu

--- car_crashers_ui/src/components/ui/info/info.js
+++ car_crashers_ui/src/components/ui/info/info.js
@@ -13,7 +13,7 @@ function Info() {
         <div className="row g-4">
           
           <div className="col-12 col-md-6 col-lg-3">
-            <div className="card h-100 border-0 shadow-sm p-4 rounded-3">
+            <div className="card h-100 border-0 shadow-sm p-4 rounded-3 feature-card">
               <div className="d-flex align-items-center justify-content-center rounded mb-3 icon-box bg-custom-blue">
                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24" stroke="white" strokeWidth={2}>
                   <path strokeLinecap="round" strokeLinejoin="round" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
@@ -25,7 +25,7 @@ function Info() {
           </div>
 
           <div className="col-12 col-md-6 col-lg-3">
-            <div className="card h-100 border-0 shadow-sm p-4 rounded-3">
+            <div className="card h-100 border-0 shadow-sm p-4 rounded-3 feature-card">
               <div className="d-flex align-items-center justify-content-center rounded mb-3 icon-box bg-custom-green">
                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24" stroke="white" strokeWidth={2}>
                   <path strokeLinecap="round" strokeLinejoin="round" d="M13 7h8m0 0v8m0-8l-8 8-4-4-6 6" />
@@ -37,7 +37,7 @@ function Info() {
           </div>
 
           <div className="col-12 col-md-6 col-lg-3">
-            <div className="card h-100 border-0 shadow-sm p-4 rounded-3">
+            <div className="card h-100 border-0 shadow-sm p-4 rounded-3 feature-card">
               <div className="d-flex align-items-center justify-content-center rounded mb-3 icon-box bg-custom-orange">
                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24" stroke="white" strokeWidth={2}>
                   <path strokeLinecap="round" strokeLinejoin="round" d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z" />
@@ -49,7 +49,7 @@ function Info() {
           </div>
 
           <div className="col-12 col-md-6 col-lg-3">
-            <div className="card h-100 border-0 shadow-sm p-4 rounded-3">
+            <div className="card h-100 border-0 shadow-sm p-4 rounded-3 feature-card">
               <div className="d-flex align-items-center justify-content-center rounded mb-3 icon-box bg-custom-purple">
                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24" stroke="white" strokeWidth={2}>
                   <path strokeLinecap="round" strokeLinejoin="round" d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z" />
