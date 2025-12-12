447f3547ec2a102b12bbaf233a182a62a75b4e58
aezkerrabe25 <aezkerrabe25@ikzubirimanteo.com>
2025-12-12 11:19:05 +0100

    argazkiak igotzeko karta egokitu

--- car_crashers_ui/src/components/ui/forms/argazkiForm.js
+++ car_crashers_ui/src/components/ui/forms/argazkiForm.js
@@ -10,9 +10,9 @@ const ArgazkiForm = ({ handleFileChange, fotos = [] }) => {
             <input type="file" id="fotosVehiculo" className="d-none" multiple accept="image/*" onChange={handleFileChange}/>
             
             <label htmlFor="fotosVehiculo" className="upload-area-label d-flex flex-column align-items-center justify-content-center p-5 rounded">
-                <span className="upload-icon">📷</span>
-                
-                <h5 className="fw-bold text-primary mb-1">Egin klik argazkiak igotzeko</h5>
+                <i class="bi bi-upload fs-1"></i>
+
+                <h5 className="fw-bold text-secondary mb-1">Egin klik argazkiak igotzeko</h5>
                 <p className="text-muted small mb-0">Igo angelu ezberdinetako argazkiak balorazio hobea lortzeko.</p>
             </label>
         </div>
