b390c5071e040c09fae96003ea9fed013782d242
OmarAkhamlich <oakhamlichco24wg@ikzubirimanteo.com>
2025-12-18 10:01:12 +0100

    koloerak aldatuz

--- car_crashers_ui/src/components/ui/goikoa/goikoa.js
+++ car_crashers_ui/src/components/ui/goikoa/goikoa.js
@@ -1,7 +1,9 @@
+import './goikoa.css';
+
 function goikoa({ children }) {
   return (
     <div className="container-fluid">
-        <div className="row justify-content-center bg-secondary text-white">
+        <div className="row justify-content-center bg-berria text-black">
           <div className="col-12 col-md-8 py-5 text-center text-md-start">
              {children}
           </div>
