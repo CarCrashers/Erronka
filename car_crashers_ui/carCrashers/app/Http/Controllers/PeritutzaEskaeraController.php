f6ae59c8e3a56a67dac952b7206c97e0ef802d50
OmarAkhamlich <oakhamlichco24wg@ikzubirimanteo.com>
2026-01-13 12:31:41 +0100

    peritutza Eskaera sortuta eta balidatuta

---
+++ car_crashers_ui/carCrashers/app/Http/Controllers/PeritutzaEskaeraController.php
@@ -0,0 +1,29 @@
+<?php
+
+use App\Http\Requests\SalduRequest;
+use App\Models\PeritutzaEskaera;
+
+class PeritutzaEskaeraController extends Controller
+{
+    public function store(SalduRequest $request)
+    {
+        $data = $request->validated();
+
+        // hemen mapatu form-eko eremuak taulara
+        PeritutzaEskaera::create([
+            'erab_id'       => auth()->id(),
+            'desguazatzeko' => $data['options-outlined'],  // bool
+            'matrikula'     => $data['matrikula'],
+            'marka'         => $data['marka'],
+            'modelo'        => $data['modelo'],
+            'urtea'         => $data['urtea'],
+            'egoera_kotxe'  => $data['egoera'],
+            'desk'          => $data['deskribapena'] ?? null,
+            // gainerako eremuak zure logikaren arabera
+        ]);
+
+        // argazkien kudeaketa aparte (Storage, erlazio taula, etab.)
+
+        return back()->with('success', 'Eskaera ongi bidalia.');
+    }
+}
