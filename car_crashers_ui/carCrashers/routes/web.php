ed50a0e104134a651314a32e00a3440405691e88
jnietoib <jnietoib24wg@ikzubirimanteo.com>
2026-02-02 13:02:34 +0100

    Pieza eta Kotxeak konponenteak + konponenteak datu baseko datuekin bete

--- car_crashers_ui/carCrashers/routes/web.php
+++ car_crashers_ui/carCrashers/routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\CarController;
 use App\Http\Controllers\PeritutzaEskaeraController;
 use App\Http\Controllers\DesguazatuController;
 use App\Http\Controllers\SaskiaController;
@@ -16,9 +17,10 @@ Route::get('/norGara', function () {
     return Inertia::render('norGara');
 })->name('norGara');
 
-Route::get('/erosi', function () {
-    return Inertia::render('erosi');
-})->name('erosi');
+Route::get('/erosi', [CarController::class, 'index'])->name('cars.index');
+// Route::get('/erosi', function () {
+//     return Inertia::render('erosi');
+// })->name('erosi');
 
 Route::get('/details', function () {
     return Inertia::render('details');
