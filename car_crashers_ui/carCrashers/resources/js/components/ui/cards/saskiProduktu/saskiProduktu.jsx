1cff21351d55e5d2afd7f40d4eabc47335605170
OmarAkhamlich <oakhamlichco24wg@ikzubirimanteo.com>
2026-02-18 13:10:21 +0100

    Saskiko argazkia aldatuta

--- car_crashers_ui/carCrashers/resources/js/components/ui/cards/saskiProduktu/saskiProduktu.jsx
+++ car_crashers_ui/carCrashers/resources/js/components/ui/cards/saskiProduktu/saskiProduktu.jsx
@@ -18,10 +18,10 @@ function SaskiProduktu({ item, onEzabatu }) {
     <div className="p-3 rounded-4 border shadow-lg d-flex flex-column flex-sm-row justify-content-between align-items-start align-items-sm-center gap-3">
       <div className="d-flex flex-grow-1">
         <img 
-          src={p.argazki_nagusia ? `/storage/${p.argazki_nagusia}` : '/images/placeholder.png'} 
+          src={p.argazki_nagusia ? `${p.argazki_nagusia}` : '/images/placeholder.png'} 
           alt={izena}
           className="me-3 rounded shadow-sm"
-          style={{width: '90px', height: '90px', objectFit: 'cover'}}
+          style={{width: '90px', height: '90px', objectFit: 'scale'}}
         />
         <div className="d-flex flex-column flex-grow-1">
           <h5 className="mb-1">{izena}</h5>
